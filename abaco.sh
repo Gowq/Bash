d942100f55ebfb5fe15b7bbe5bdd625665e7a348
Gowq <cardozo@usp.br>
2019-01-15 20:21:20 -0200

    Implementing program main loop

--- abaco.sh
+++ abaco.sh
@@ -1,22 +1,29 @@
 begin() {
+    echo
     read -p 'How many digits [0-5]: ' dig
     alg=$[10 ** $dig]
-    let num1=$RANDOM%$alg
-    let num2=$RANDOM%$alg
+    let min=$[10 ** $dig-2]
+    let num1=$RANDOM%$alg+min
+    let num2=$RANDOM%$alg+min
     let res=$num1+$num2
+    if [ $dig -ne 0 ]
+    then    
+        func
+    else 
+        exit
+    fi
 }
 
 func() {
     echo $num1 + $num2:  
     read pal
-    echo 
     if [ $pal -ne $res ]
     then   
         echo Incorrect
         func
+    else 
+        echo Correct
+        begin
     fi
 }
 begin
-func
-echo Correct
-
